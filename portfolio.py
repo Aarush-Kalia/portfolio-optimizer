f184c344a53c3daf7ea4a3238a2cba655fccc3d4
Aarush Kalia <aarushkalia@Aarushs-MacBook-Air-9.local>
2026-08-27 21:08:40 -0400

    frontier curve complete

--- portfolio.py
+++ portfolio.py
@@ -49,14 +49,39 @@ def min_volatility_for_target(mean_returns, cov_matrix, target_return):
     result = minimize(objective_min_vol, initial_guess, method = 'SLSQP', constraints = cons, bounds = bounds, args = (mean_returns, cov_matrix))
     return result.x
 
+def efficient_frontier(mean_returns, cov_matrix, num_points = 20):
+    mean_returns = np.asarray(mean_returns)
+    target_values = np.linspace(min(mean_returns), max(mean_returns), num_points)
+    vol_list = []
+    return_list = []
+    ret_vol_pair = []
+    for value in target_values:
+        min_vol_weight = min_volatility_for_target(mean_returns, cov_matrix, value)
+        stats = portfolio_stats(min_vol_weight, mean_returns, cov_matrix, risk_free_rate=.02)
+        vol_list.append(stats[1])
+        return_list.append(stats[0])
+
+    for i in range(len(vol_list)):
+        ret_vol_pair.append((vol_list[i], return_list[i]))
+
+    smallest_index = np.argmin(vol_list)
+    return ret_vol_pair[smallest_index:]
+    
+
+
 if __name__ == "__main__":
     mean_returns = [.08, .12, .10]
     cov_matrix = [[.0225, .01125, 0.015], [.01125, .0625, -0.01], [.015, -0.01, .04]]
-    weights = max_sharpe_scipy(mean_returns, cov_matrix, risk_free_rate=0.02)
+    #weights = max_sharpe_scipy(mean_returns, cov_matrix, risk_free_rate=0.02)
     #print(weights)
 
-    min_vol = min_volatility_for_target(mean_returns, cov_matrix, target_return=.095)
+    #min_vol = min_volatility_for_target(mean_returns, cov_matrix, target_return=.095)
     #print(min_vol)
 
-    weights = [0.625, 0.375, .0]
-    print(portfolio_stats(weights, mean_returns, cov_matrix, risk_free_rate = 0.02)[1])
+    #weights = [0.625, 0.375, .0]
+    #print(portfolio_stats(weights, mean_returns, cov_matrix, risk_free_rate = 0.02)[1])
+
+    frontier = efficient_frontier(mean_returns, cov_matrix, num_points=20)
+    print('  vol     ret')
+    for vol, ret in frontier:
+        print(f"{vol:.5f} {ret:.5f}")
