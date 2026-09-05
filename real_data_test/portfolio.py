7ca835493a4b82f5ac05dd9efbb043bce4abef2c
Aarush Kalia <aarushkalia@Aarushs-MacBook-Air-9.local>
2026-09-05 17:44:41 -0400

    comparison function complete

--- real_data_test/portfolio.py
+++ real_data_test/portfolio.py
@@ -3,6 +3,7 @@ import pandas as pd
 import yfinance as yf
 from scipy.optimize import minimize
 from data_loader import stock_data
+import matplotlib.pyplot as plt
 
 def portfolio_stats(weights, mean_returns, cov_matrix, risk_free_rate = 0.02):
     weights = np.asarray(weights)
@@ -82,16 +83,68 @@ def backtest(tickers, start, end, lookback = 756, rebalance_freq = 63, risk_free
         cov_matrix = daily_returns.iloc[i - lookback : i, :].cov()
 
         weights = max_sharpe_scipy(mean_returns, cov_matrix, risk_free_rate)
-        actual_return.extend(np.dot(daily_returns.iloc[i : i+rebalance_freq, :], weights))
-
-    num_rebalances = len(range(lookback, len(daily_returns) - rebalance_freq + 1, rebalance_freq))
-    predicted_length = num_rebalances * rebalance_freq
-    print(predicted_length)
-    print(len(actual_return))
+        paired = zip(np.dot(daily_returns.iloc[i:i+rebalance_freq, :], weights), daily_returns.iloc[i:i+rebalance_freq, :].index)
+        actual_return.extend(paired)
 
     return actual_return
 
+def one_dol_growth(tickers, start, end, lookback = 756, rebalance_freq = 63, risk_free_rate = 0.02):
+    backtest_info = backtest(tickers, start, end, lookback, rebalance_freq, risk_free_rate)
 
+    cumul_plot = [x for x,y in backtest_info]
+    cumul_plot = np.asarray(cumul_plot)
+    cumul_return = np.cumprod(1 + cumul_plot)
+    dates = [y for x,y in backtest_info]
+    plt.plot(dates, cumul_return, color="#ff0000", linewidth=1.25)
+    plt.show()
+
+def backtest_summary(returns, risk_free_rate = 0.02):
+    returns = np.asarray(returns)
+    cumul_return = np.cumprod(1 + returns)
+    annual_vol = np.std(returns) * np.sqrt(252)
+    annual_return = cumul_return[-1] ** (252/len(returns)) - 1
+    sharpe = (annual_return - risk_free_rate) / annual_vol
+
+    return annual_return, annual_vol, sharpe
+
+def benchmark_comparison(tickers, start, end, lookback = 756, rebalance_freq = 63):
+    my_strat = backtest(tickers, start, end, lookback, rebalance_freq)
+    equal_weight = equal_weight_backtest(tickers, start, end, lookback)
+    SPY_comparison = SPY_backtest(start, end)
+
+    strategy_dates = [date for ret, date in my_strat]
+    equal_weight_dates = [date for ret, date in equal_weight]
+    spy_dates = [date for ret, date in SPY_comparison]
+
+    common_dates = set(strategy_dates) & set(equal_weight_dates) & set(spy_dates)
+
+    strategy_filtered = [pair for pair in my_strat if pair[1] in common_dates]
+    equal_weight_filtered = [pair for pair in equal_weight if pair[1] in common_dates]
+    spy_filtered = [pair for pair in SPY_comparison if pair[1] in common_dates]
+
+    return strategy_filtered, equal_weight_filtered, spy_filtered
+
+def equal_weight_backtest(tickers, start, end, lookback=756):
+    data = yf.download(tickers, start, end)
+    closing_prices = data["Close"]
+    df = pd.DataFrame(closing_prices)
+    daily_returns = df.pct_change().dropna()
+
+    weights = [1 / len(tickers)] * len(tickers)
+    sliced = daily_returns.iloc[lookback:, :]
+
+    paired = zip(np.dot(sliced, weights), sliced.index)
+    return list(paired)
+
+def SPY_backtest(start, end, lookback = 756):
+    data = yf.download("SPY", start, end)
+    closing_prices = data["Close"]
+    df = pd.DataFrame(closing_prices)
+    daily_returns = df.pct_change().dropna()
+
+    paired = zip(daily_returns["SPY"].iloc[lookback:], daily_returns.index[lookback:])
+
+    return list(paired)
 
 
 if __name__ == "__main__":
@@ -106,13 +159,24 @@ if __name__ == "__main__":
     #print(portfolio_stats(weights, mean_returns, cov_matrix, risk_free_rate = 0.02)[1])
 
     
-    mean_returns, cov_matrix = stock_data(["AAPL", "JNJ", "XOM"], "2000-01-01", "2024-01-01")
+    mean_returns, cov_matrix = stock_data(["CI", "UNH", "CVS"], "2000-01-01", "2024-01-01")
     weights = max_sharpe_scipy(mean_returns, cov_matrix, risk_free_rate=0.02)
     frontier = efficient_frontier(mean_returns, cov_matrix, num_points=20)
+
+    #print(portfolio_stats(weights, mean_returns, cov_matrix)[2])
 
     #print(weights)
     #print(' vol    ret')
     #for vol, ret in frontier:
         #print(f"{vol:.3f} {ret:.3f}")
 
-    backtest(["AAPL", "JNJ", "XOM"], "2000-01-01", "2024-01-01")
+    #print(one_dol_growth(["CI", "UNH", "CVS"], "2000-01-01", "2024-01-01"))
+
+    returns = [x for x, y in backtest(["CI", "UNH", "CVS"], "2000-01-01", "2024-01-01")]
+    annual_return, annual_vol, sharpe = backtest_summary(returns, risk_free_rate=0.02)
+    #print(f"Annualized Return: {annual_return:.4f}")
+    #print(f"Annualized Volatility: {annual_vol:.4f}")
+    #print(f"Sharpe Ratio: {sharpe:.4f}")
+
+    compare = benchmark_comparison(["AAPL", "JNJ", "XOM"], "2000-01-01", "2024-01-01")
+    print(len(compare[0]), len(compare[1]), len(compare[2]))
